d98d656011b8dd7463bfc3710e235bebf02b1e8e
Atabong Cecilia <catabong89@gmail.com>
2023-11-04 22:04:03 +0100

    implemented footer section

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,15 +4,16 @@ const Footer = () => {
     return ( 
         <section className="footer">
             <div>
+                <p>Follow us on social media</p>
                 <ul>
                     <li><ion-icon name="logo-instagram"></ion-icon></li>
                     <li><ion-icon name="logo-facebook"></ion-icon></li>
-                    <li><ion-icon name="logo-snapchat"></ion-icon></li>
+                    <li><ion-icon name="logo-youtube"></ion-icon></li>
                     <li><ion-icon name="logo-twitter"></ion-icon></li>
                 </ul>
             </div>
 
-            <div>Privacy plicy</div>
+            <div>Privacy policy</div>
         </section>
      );
 }
